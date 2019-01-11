0a62db014af942025dfab8df2dcc0c0e25426f71
Nuno Godinho <user@nunogodinho.com>
2019-01-11 01:35:53 +0100

    Trying to convert to Retina but still without success
    This is the moment I asked for help in the forum:
    https://forum.openframeworks.cc/t/retina-issue-on-macos-bug-or-ignorance/31492

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,18 +3,20 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-  ofSetFrameRate(60);
+  ofSetFrameRate(30);
   ofSetVerticalSync(true);
-
   Ink *ink = new InkColor(ofColor(0,0,255));
   Family *f = new Family(ink, ofVec2f(200,100), 50);
-  Surface *s = new Surface(f, "images/test_texture.jpg", ofVec2f(ofGetWidth(), ofGetHeight()));
-  ms = new MaskedSurface(s, "images/bread_profile_mask.png");
+  Surface *s = new Surface(f, "images/test_texture_retina.jpg", ofVec2f(ofGetWidth(), ofGetHeight()));
+  ms = new MaskedSurface(s, "images/bread_profile_mask_retina.png");
+
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+  ofLog() << ofGetWidth();
   ms->update();
+  
 }
 
 //--------------------------------------------------------------
