9d790e701f469d32b95419df934e3461c950a0dc
Nuno Godinho <user@nunogodinho.com>
2019-02-24 17:17:59 +0000

    Set fullscreen by default

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -3,7 +3,7 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(2880,1620,OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(2880,1620,OF_FULLSCREEN);			// <-------- setup the GL context
   // 4K: 3840 x 2160
 
   // this kicks off the running of my app
