a1b0288b1fa716dc69127f62b4daa77375e55408
Nuno Godinho <user@nunogodinho.com>
2019-03-02 12:26:46 +0000

    Add Tools.getRandomVec2f()

--- src/app/Tools.h
+++ src/app/Tools.h
@@ -16,6 +16,7 @@ public:
   static float posToAngle(ofVec2f v) {return ofVec2f(1,0).angle(v);}
   static float randomInRange(ofVec2f v) {return ofRandom(v.x, v.y);}
   static float randomInSet(vector<float> set);
+  static ofVec2f getRandomVec2f() {ofVec2f(ofRandom(99999), ofRandom(99999));}
 };
 
 #endif /* Tools_h */
